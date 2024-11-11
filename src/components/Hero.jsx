8bace69f8a88062ac4ecb7bd7d5db721341f5953
hamdifreelance <hamdifreelance365@gmail.com>
2024-11-11 15:02:25 +0100

    ai summarizer project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/hamdifreelance/ai_summarizer", "_blank")
           }
           className='black_btn'
         >
